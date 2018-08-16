263ace60b27df06adb4f4f8e18635e55c3111bf3
Pawel Bus <pawelbus.dev@outlook.com>
2018-08-16 11:27:43 +0200

    Fix background url-loader in webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -84,7 +84,7 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 10000,
+              limit: 30000
             },
           },
         ],
